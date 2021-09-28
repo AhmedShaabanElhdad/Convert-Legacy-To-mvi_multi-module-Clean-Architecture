195f229feac94c20de1abfed39adb943ac6dfd20
AhmedShaabanElhdad <ahmed.shaaban.elhdah@gmail.com>
2021-09-28 13:56:00 +0200

    - make nav controller with navigation will be assign later in feature module - we use value id to assign the navigation in feature with them instead of using navigation in app module - use deeplinking to move from one fragment in module to another one in another module

---
+++ navigation/src/main/java/com/example/navigation/DeepLinkingNavControl.kt
@@ -0,0 +1,34 @@
+package com.example.navigation
+
+import androidx.core.net.toUri
+import androidx.navigation.NavController
+import androidx.navigation.NavDeepLinkRequest
+import androidx.navigation.NavOptions
+
+fun buildDeepLink(destination: DeepLinkDestination) =
+    NavDeepLinkRequest.Builder
+        .fromUri(destination.address.toUri())
+        .build()
+
+fun NavController.deepLinkNavigateTo(
+    deepLinkDestination: DeepLinkDestination,
+    popUpTo: Boolean = false
+) {
+    val builder = NavOptions.Builder()
+
+    if (popUpTo) {
+        builder.setPopUpTo(graph.startDestination, true)
+    }
+
+    navigate(
+        buildDeepLink(deepLinkDestination),
+        builder.build()
+    )
+}
+
+const val DOMAIN = "example://"
+
+sealed class DeepLinkDestination(val address: String) {
+    object Auth : DeepLinkDestination("${DOMAIN}auth")
+    object Product : DeepLinkDestination("${DOMAIN}product")
+}
