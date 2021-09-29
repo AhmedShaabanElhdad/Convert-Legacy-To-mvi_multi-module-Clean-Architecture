5c86f1697ee15b38e6e9187c667c0cfa6f59b000
AhmedShaabanElhdad <ahmed.shaaban.elhdah@gmail.com>
2021-09-29 15:48:01 +0200

    - add productRepository implementation that return flow of list of products

---
+++ data/src/main/java/com/example/data/repositoryImp/ProductRepoImp.kt
@@ -0,0 +1,41 @@
+package com.example.data.repositoryImp
+
+import com.example.data.network.HalanService
+import com.example.data.pref.SharedPref
+import com.example.data.pref.TOKEN
+import com.example.domain.core.ViewState
+import com.example.domain.repository.ProductRepo
+import com.example.domain.repository.UserRepo
+import com.example.entity.Product
+import com.example.entity.Profile
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+class ProductRepoImp @Inject constructor(
+    private val service: HalanService,
+    private val pref: SharedPref
+) : ProductRepo {
+
+
+    override fun getUserProduct(): Flow<ViewState<List<Product>>> {
+        return flow {
+            try {
+
+                emit(ViewState.Loading)
+                val response = service.getProduct("")
+                response.body()?.apply {
+                    if (response.code() == 200 && this.status == "OK") {
+                        emit(ViewState.Success(this.products))
+                    } else
+                        emit(ViewState.Error(response.message()))
+                }
+
+            } catch (e: Exception) {
+                emit(ViewState.Error(e.message ?: "Error"))
+            }
+        }.flowOn(Dispatchers.IO)
+    }
+}
